a3d449bac87f794bbc8ab3fca0c2d9ffe575135d
Saubhik Kumar <103452749+Saubhik-Kumar-Dash@users.noreply.github.com>
2025-08-03 11:12:41 +0530

    Add files via upload

---
+++ p_vscode_shortcut.py
@@ -0,0 +1,22 @@
+"""multiline comment "#": 
+        select the lines and place cursor at the start then press ctrl + /
+        to remove it, press it again"""
+
+# jbjbcjcdj
+# jjbcjemk cmcemcee
+# ec ekmc,l c cecelc,,celmcmcm
+
+"""multiline cursor: 
+        press and hold alt then select the lines you want the cursor"""
+
+"""command palette: 
+        Access all available commands based on your current context. 
+        Many of the commands in the Command Palette have default keyboard shortcuts 
+        associated with them. You can see the default keyboard shortcut alongside 
+        the command in the Command Palette 
+        Ctrl+Shift+P"""
+
+
+"""Open CMD for any directory:
+        1st. open the location where you want to use the cmd using windows explorer
+        2nd. On the file location, write cmd then enter"""
